4de1a54e24a02a98ff3f341334487654296f6232
sylow <glenacsalim@gmail.com>
2023-12-07 20:54:06 +0100

    mise à jour DATA

--- data/data.js
+++ data/data.js
@@ -1,36 +1,5 @@
 const comptes = {
     "Sylow_prod": {
         "bio": "MMMhhh, juste une bio ! Afficher en JS grâce à une DATA",
-        "description": "Cette SAE doit amener les étudiants à utiliser des outils d’audit objectifs comme les guides de bonne pratique ou les référentiels de qualité pour évaluer un site web.<br>Elle met en pratique les concepts théoriques explorés dans les ressources liées à la culture numérique, à la stratégie de communication et à l’économie.<br>Elle permet la mise en oeuvre des outils d’analyse statistique sur des données variées : données de trafic, sondages d’opinion ou études socio-économiques.",
-        "Post": {
-            "AC11.01": "Présenter une organisation, ses activités et son environnement (économique, sociologique, culturel, juridique, technologique, communicationnel et médiatique)",
-            "AC11.02": "Évaluer un site web, un produit multimédia ou un dispositif interactif existant en s’appuyant sur des guides de bonnes pratiques",
-            "AC11.03": "Produire des analyses statistiques descriptives et les interpréter pour évaluer un contexte socio-économique",
-            "AC11.04": "Analyser des formes médiatiques et leur sémiotique",
-            "AC11.05": "Identifier les cibles (critères socio-économiques, démographiques, géographiques, culturels...)"
-        },
-        "ressources": {
-            "R1.03": "Ergonomie et Accessibilité",
-            "R1.04": "Culture numérique",
-            "R1.05": "Stratégies de communication et marketing",
-            "R1.09": "Culture artistique",
-            "R1.14": "Représentation et traitement de l’information",
-            "R1.16": "Économie, gestion et droit du numérique"
-        },
     },
-
-    "Test": {
-        "bio": "Concevoir une recommandation de communication numérique",
-        "description": "En tant que chargés de communication juniors, les étudiants conçoivent une recommandation de communication pour la sortie ou le repositionnement d’un produit ou d’un service dans un contexte international.",
-        "Post": {
-            "AC12.03": "Proposer une recommandation marketing (cibles, objectifs, points de contact)",
-            "AC12.04": "Proposer une stratégie de communication"
-        },
-        "ressources": {
-            "R1.01": "Anglais",
-            "R1.02": "Anglais Renforcé ou LV2",
-            "R1.05": "Stratégies de communication et marketing",
-            "R1.06": "Expression, communication et rhétorique"
-        },
-    }
 }
