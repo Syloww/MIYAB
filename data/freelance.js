9f51b22cc9955c813c875bcf956c483e3ffdec55
sylow <glenacsalim@gmail.com>
2023-12-08 14:55:40 +0100

    anim text

---
+++ data/freelance.js
@@ -0,0 +1,74 @@
+const freelancers = {
+    "photographes": {
+        "photographe1": {
+            "nom": "Alice Photographe",
+            "specialite": "Photographie de portraits",
+            "experience": "10 ans d'expérience dans la photographie de portraits et d'événements.",
+            "tarif": "100€/heure",
+            "disponibilite": "Disponible pour des séances photos personnalisées.",
+            "portfolio": ["lien_portfolio_1", "lien_portfolio_2"],
+        },
+        "photographe2": {
+            "nom": "Bob Photographe de mariage",
+            "specialite": "Photographie de mariage",
+            "experience": "Spécialisé dans la capture d'émotions lors des mariages.",
+            "tarif": "150€/heure",
+            "disponibilite": "Disponible pour des mariages et événements spéciaux.",
+            "portfolio": ["lien_portfolio_1", "lien_portfolio_2"],
+        },
+    },
+    "videastes": {
+        "videaste1": {
+            "nom": "Charlie Vidéaste événementiel",
+            "specialite": "Vidéographie événementielle",
+            "experience": "Capturer des moments spéciaux lors d'événements et de conférences.",
+            "tarif": "120€/heure",
+            "disponibilite": "Disponible pour couvrir divers événements.",
+            "portfolio": ["lien_portfolio_1", "lien_portfolio_2"],
+        },
+        "videaste2": {
+            "nom": "David Réalisateur de clips musicaux",
+            "specialite": "Réalisation de clips musicaux",
+            "experience": "Collaboration avec des artistes pour créer des vidéos créatives.",
+            "tarif": "200€/heure",
+            "disponibilite": "Ouvert à des projets musicaux innovants.",
+            "portfolio": ["lien_portfolio_1", "lien_portfolio_2"],
+        },
+    },
+    "monteurs": {
+        "monteur1": {
+            "nom": "Eva Monteur publicitaire",
+            "specialite": "Montage vidéo publicitaire",
+            "experience": "Création de montages percutants pour des campagnes publicitaires.",
+            "tarif": "80€/heure",
+            "disponibilite": "Disponible pour des projets publicitaires.",
+            "portfolio": ["lien_portfolio_1", "lien_portfolio_2"],
+        },
+        "monteur2": {
+            "nom": "Frank Monteur de films documentaires",
+            "specialite": "Montage de films documentaires",
+            "experience": "Travail sur des documentaires captivants et informatifs.",
+            "tarif": "150€/heure",
+            "disponibilite": "Ouvert à des collaborations sur des projets documentaires.",
+            "portfolio": ["lien_portfolio_1", "lien_portfolio_2"],
+        },
+    },
+    "inge_son": {
+        "inge_son1": {
+            "nom": "Grace Ingénieur du son studio",
+            "specialite": "Ingénierie du son en studio",
+            "experience": "Enregistrement et mixage professionnel en studio.",
+            "tarif": "120€/heure",
+            "disponibilite": "Disponible pour des sessions d'enregistrement.",
+            "portfolio": ["lien_portfolio_1", "lien_portfolio_2"],
+        },
+        "inge_son2": {
+            "nom": "Harry Ingénieur du son live",
+            "specialite": "Sonorisation live",
+            "experience": "Travail sur des événements en direct et des concerts.",
+            "tarif": "150€/heure",
+            "disponibilite": "Disponible pour des événements en direct et des tournées.",
+            "portfolio": ["lien_portfolio_1", "lien_portfolio_2"],
+        },
+    },
+};
---
+++ js/anim.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Redirige vers une autre page après 5 secondes
+    setTimeout(function() {
+        window.location.href = "menu.html";
+    }, 5000);
+});
