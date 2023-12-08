17aabab8065c97d7ab1fe4cf37282a3bc02c891a
sylow <glenacsalim@gmail.com>
2023-12-08 16:13:28 +0100

    data amélioré et relié
    yes

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,19 @@
 let recherche = document.querySelector("#search").value;
 
-function Freelance(){
-    document.querySelector(".titrepage").innerHTML= "Freelance";
+function Freelance() {
+    document.querySelector(".titrepage").innerHTML = "Freelance";
+    let liste2 = '';
+
+    for (let category in freelancers) {
+        for (let freelancer in freelancers[category]) {
+            liste2 += '<div class="allpost">' + '<div class="post">' + '<div class="info">' + '<div class="nom">' + freelancers[category][freelancer]["nom"] + '</div>' + '<div class="spe">' + freelancers[category][freelancer]["specialite"] + '</div>' + '<div class="tarif"><span>' + freelancers[category][freelancer]["tarif"] + '</span></div><div class="descri">' + freelancers[category][freelancer]["description"] + '</div><div class="descri"><span>' + freelancers[category][freelancer]["disponibilite"] + '</span></div></div></div></div>';
+        }
+    }
+    document.querySelector(".allpost").innerHTML = liste2
 }
-function Projet(){
-    document.querySelector(".titrepage").innerHTML= "Projet";
+function Projet() {
+    document.querySelector(".titrepage").innerHTML = "Projet";
+    document.querySelector(".allpost").innerHTML = ""
 }
+
+
