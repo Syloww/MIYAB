a16d0c4ff6b4ba34fdefb644319d5ee77523794b
sylow <glenacsalim@gmail.com>
2023-12-08 15:21:29 +0100

    miseajouranim
    durée

--- js/anim.js
+++ js/anim.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function() {
     // Redirige vers une autre page après 5 secondes
     setTimeout(function() {
         window.location.href = "menu.html";
-    }, 5000);
+    }, 3000);
 });
