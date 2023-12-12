63957dbbcdff8e8452693c931bd6990f96cdcca0
sylow <glenacsalim@gmail.com>
2023-12-12 21:28:59 +0100

    optimisation du script
    script

--- js/profil.js
+++ js/profil.js
@@ -1,32 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const pseudoElement = document.querySelector('.pseudo');
-    const bioElement = document.querySelector('.bio');
-    const profilePictureElement = document.querySelector('.pp');
+document.addEventListener("DOMContentLoaded", function () {
+    const elements = {
+        pseudo: document.querySelector('.pseudo'),
+        bio: document.querySelector('.bio'),
+        pp: document.querySelector('.pp')
+    };
 
-    // Récupérer le nom du compte dans le fichier profil.html
     const params = new URLSearchParams(window.location.search);
     const selectedAccount = params.get('account');
+    const accountData = selectedAccount ? comptes[selectedAccount] : null;
 
-    // Mettre à jour le texte avec la classe "pseudo" dans le fichier profil.html
-    if (selectedAccount && pseudoElement) {
-        pseudoElement.textContent = selectedAccount;
+    if (elements.pseudo) {
+        elements.pseudo.textContent = selectedAccount || "Nom d'utilisateur non disponible";
     }
 
-    // Mettre à jour le texte avec la classe "bio" dans le fichier profil.html
-    if (selectedAccount && bioElement) {
-        const bioText = comptes[selectedAccount] ? comptes[selectedAccount].bio : "Biographie non disponible";
-        bioElement.textContent = bioText;
+    if (elements.bio) {
+        elements.bio.textContent = accountData ? accountData.bio : "Biographie non disponible";
     }
 
-    // Mettre à jour l'image avec la classe "pp" dans le fichier profil.html
-    if (selectedAccount && profilePictureElement) {
-        let profilePictureSrc = comptes[selectedAccount] ? comptes[selectedAccount].profilePicture : "";
-
-        // Utiliser l'image par défaut si profilePictureSrc est une chaîne vide ou non définie
-        if (!profilePictureSrc) {
-            profilePictureSrc = "./profil_pp/default.png"; // Assurez-vous que le chemin est correct
-        }
-
-        profilePictureElement.src = profilePictureSrc;
+    if (elements.pp) {
+        const profilePictureSrc = accountData ? accountData.profilePicture || "./profil_pp/default.png" : "./profil_pp/default.png";
+        elements.pp.src = profilePictureSrc;
     }
 });
